cb48fd696285c90e1fb3465bac17a06f3271d3d9
minooo <x13603983223@163.com>
2019-01-16 17:38:14 +0800

    base

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-    <div class="bg-main c-white plr20 ptb15 f18 bold">页面构造器</div>
+    <div class="bg-main c-white plr20 ptb15 f18 bold">页面构造器（v0.0.1）</div>
     <div class="flex plr20">
       <div class="f110 o-h r4 mr20">
-        <div class="f24 bold lh150 ptb20 b-b mb20">生成的页面</div>
-        <div class="border r4">
-          <div class="f20 lh150 ptb10 b-b ml10 mr10">页面</div>
-          <div :style="{minHeight: '150px'}"></div>
-        </div>
-      </div>
-      <div class="f110 o-h r4">
-        <div class="f24 bold lh150 ptb20 b-b mb20">拖拽下面的组件到左侧</div>
+        <div class="f24 bold lh150 ptb20 b-b mb20">组件列表区</div>
         <div class="flex fd-c ai-c">
-          <div>
+          <draggable
+            style="width: 100px"
+            class="flex fd-c ai-c ptb20 move"
+            @start="onDragStart"
+            @end="onDragEnd"
+          >
+            <span>文本框</span>
+          </draggable>
+
+          <div class="plr20 ptb20">
             <span>标题</span>
             <Input v-model="value" placeholder="Enter something..." style="width: 300px"/>
           </div>
+        </div>
+      </div>
+
+      <div class="f110 o-h r4">
+        <div class="f24 bold lh150 ptb20 b-b mb20">实时预览区</div>
+        <div class="border r4">
+          <div class="f20 lh150 ptb10 b-b ml10 mr10">页面</div>
+          <draggable
+            :style="{minHeight: '150px'}"
+            :options="fromOptions"
+            class="ptb20 plr20"
+            @start="drag=true"
+            @end="drag=false"
+          ></draggable>
         </div>
       </div>
     </div>
@@ -24,14 +40,31 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
+import draggable from 'vuedraggable'
 
 export default {
   components: {
-    Logo
+    Logo,
+    draggable
   },
   data() {
     return {
-      value: ''
+      value: '',
+      single: false,
+      fromOptions: {
+        group: {
+          name: 'components',
+          pull: 'clone' // To clone: set pull to 'clone'
+        }
+      }
+    }
+  },
+  methods: {
+    onDragStart: function(e) {
+      console.log('start', e)
+    },
+    onDragEnd: function(e) {
+      console.log('end', e)
     }
   }
 }
